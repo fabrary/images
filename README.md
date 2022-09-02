15c4af54352ae16897a6e1a558e7f5c727ccb76f
Phillip Manwaring <phillip.manwaring@gmail.com>
2022-09-01 21:08:38 -0400

    add readme instructions

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# images
+# FaB Images
+
+Initial image setup from: https://github.com/flesh-cube/flesh-and-blood-cards/tree/main/download-all-images
+
+### Project setup requirements
+
+- Nodejs
+- MacOS ARM64 (for webp image conversion, or add compatible binaries for other OS)
+- `npm i` to install Nodejs dependencies
+
+### Spoilers card image conversion steps
+
+- Add images to `src/dynasty`
+- `npm run convert:dynasty`
+
+### Released card image conversion steps
+
+- Add images from https://github.com/flesh-cube/flesh-and-blood-cards/tree/main/download-all-images to `src/cards`
+- `npm run convert`
+
+### Hero card image conversion steps
+
+- For hero avatar with decks
+- Create/crop 250 x 250 px jpeg image framing hero
+- Add image to `src/heroes`
+- `npm run convert:heroes`
--- convert-to-webp.js
+++ convert-to-webp.js
@@ -1,5 +1,4 @@
 const fs = require("fs");
-const path = require("path");
 const glob = require("glob");
 const sharp = require("sharp");
 
@@ -7,17 +6,24 @@ const cardSource = "src/cards";
 const cardDestination = "webp/cards";
 const heroSource = "src/heroes";
 const heroDestination = "webp/heroes";
+const dynastySource = "src/dynasty";
+const dynastyDestination = "webp/dynasty";
 
 (async () => {
-  const deleteExistingImages = async () => {
+  const deleteExistingImages = async (directory) => {
     console.log("Removing existing images");
     const options = { recursive: true };
 
-    fs.rmSync(cardDestination, options);
-    fs.mkdirSync(cardDestination);
+    if (directory) {
+      fs.rmSync(directory, options);
+      fs.mkdirSync(directory);
+    } else {
+      fs.rmSync(cardDestination, options);
+      fs.mkdirSync(cardDestination);
 
-    fs.rmSync(heroDestination, options);
-    fs.mkdirSync(heroDestination);
+      fs.rmSync(heroDestination, options);
+      fs.mkdirSync(heroDestination);
+    }
   };
 
   const convertImages = async (images, destination) => {
@@ -43,11 +49,22 @@ const heroDestination = "webp/heroes";
     }
   };
 
-  await deleteExistingImages();
-
-  const cards = glob.sync(`${cardSource}/**/*`);
-  const heroes = glob.sync(`${heroSource}/**/*`);
-  console.log(`Converting ${cards.length + heroes.length} images`);
-  await convertImages(cards, cardDestination);
-  await convertImages(heroes, heroDestination);
+  if (process.env.DYNASTY_ONLY) {
+    await deleteExistingImages(dynastyDestination);
+    const dynastyCards = glob.sync(`${dynastySource}/**/*`);
+    console.log(`Converting ${dynastyCards.length} images`);
+    await convertImages(dynastyCards, dynastyDestination);
+  } else if (process.env.HEROES_ONLY) {
+    await deleteExistingImages(heroDestination);
+    const heroes = glob.sync(`${heroSource}/**/*`);
+    console.log(`Converting ${heroes.length} images`);
+    await convertImages(heroes, heroDestination);
+  } else {
+    await deleteExistingImages();
+    const cards = glob.sync(`${cardSource}/**/*`);
+    const heroes = glob.sync(`${heroSource}/**/*`);
+    console.log(`Converting ${cards.length + heroes.length} images`);
+    await convertImages(cards, cardDestination);
+    await convertImages(heroes, heroDestination);
+  }
 })();
